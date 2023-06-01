da79783ff8495fff463b11fd4c6d078fb115f4fa
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-06-01 07:26:13 +0600

    Added EsLint & Prettier 🚀

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
-import express, { Application, Request, Response, urlencoded } from "express";
-import cors from "cors";
+import express, { Application, Request, Response, urlencoded } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
-const port = 3000;
-app.use(cors());
-app.use(urlencoded({ extended: true }));
+const app: Application = express()
+const port = 3000
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working Successfully");
-});
+app.use(cors())
+app.use(urlencoded({ extended: true }))
 
-export default app;
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working Successfully')
+})
+
+export default app
